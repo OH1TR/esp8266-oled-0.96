5b4b4324884becfb40f95e637dd6322fc9e5e34e
Tommi Rouvali <tommi@rouvali.com>
2024-08-25 17:36:29 +0300

    .

---
+++ HelloWorld/HelloWorld.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.write("\r\nStart\r\n");
+  Wire.begin(5, 4);
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+}
+
+void loop() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Hello world!");
+  display.display();
+  
+  Serial.write(".");
+
+  delay(1000);
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+### ESP8266 0.96inch OLED Hello world application
+
+This is a Hello World app for a module sold on eBay: "ESP8266 0.96inch OLED Clock Weather PM2.5 Calendar ESP-12F Development Board"
+
+![](Board.png)
